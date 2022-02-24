e03e370822b4394569f69ed5ab1505330a05df73
Aung Ko Ko Ye <aungkokoye@gmail.com>
2022-02-24 15:45:26 +0000

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel Play Ground</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <nav class="container-fluid bg-gray-200 border:text-gray-400">
+        <div class="max-w-6xl mx-auto">
+            <div class="flex justify-between">
+                <div class="flex">
+
+                    <!-- Logo -->
+                    <div>
+                        <a
+                            href="#"
+                            class="flex items-center py-4 px-2"
+                        >
+                            <span class="font-semibold text-orange-500 text-lg">Laravel Playground</span>
+                        </a>
+                    </div>
+
+                    <!-- Primary Navbar items -->
+                    <div class="md:flex items-center">
+                        <a
+                            href=""
+                            class="py-4 px-2 text-gray-400 hover:text-orange-500"
+                        >
+                            Home
+                        </a>
+
+                        <a
+                            href=""
+                            class="py-4 px-2 text-gray-400 hover:text-orange-500"
+                        >
+                            Services
+                        </a>
+
+                        <a
+                            href=""
+                            class="py-4 px-2 text-gray-400 hover:text-orange-500"
+                        >
+                            About
+                        </a>
+
+                        <a
+                            href=""
+                            class="py-4 px-2 text-gray-400 hover:text-orange-500"
+                        >
+                            Contact Us
+                        </a>
+
+                    </div>
+
+                </div>
+
+                <!-- Right Navbar items -->
+                <div class="md:flex items-center space-x-1">
+                    <a
+                        href=""
+                        class="py-4 px-2 text-gray-400 hover:text-orange-500"
+                    >
+                        Login
+                    </a>
+                    <a
+                        href=""
+                        class="py-4 px-2 text-gray-400 hover:text-orange-500"
+                    >
+                        Sign Up
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
